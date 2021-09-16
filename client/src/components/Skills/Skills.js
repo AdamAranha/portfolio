036792e74b32a2bcabe362820ade581b84c41755
Adam <aearanha@hotmail.com>
2021-09-15 20:27:29 -0400

    Added Skills section

---
+++ client/src/components/Skills/Skills.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Skills.css';
+
+export default function Skills() {
+    return (
+        <div className='skills' id='skills'>
+            <div className='container'>
+
+            </div>
+        </div>
+    )
+}
