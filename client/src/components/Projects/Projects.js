f8c949a5099196d8e9879eade6357921528bfce1
Adam <aearanha@hotmail.com>
2021-09-15 20:26:07 -0400

    Created shell for Project section

--- client/src/components/Projects/Projects.js
+++ client/src/components/Projects/Projects.js
@@ -4,7 +4,9 @@ import './Projects.css';
 export default function Projects() {
     return (
         <div className="projects" id="projects">
+            <div className='container'>
 
+            </div>
         </div>
     )
 }
