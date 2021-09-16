56572a0b97ba3544927bbc8740ec32ea25b8ccbe
Adam <aearanha@hotmail.com>
2021-09-15 20:27:06 -0400

    Created Contact Section

---
+++ client/src/components/Contact/Contact.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Contact.css';
+
+export default function Contact() {
+    return (
+        <div className='contact' id='contact'>
+            <div className='container'>
+
+            </div>
+        </div>
+    )
+}
