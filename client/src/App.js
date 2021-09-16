20b2bb766313d2004f42097a3569a1f8391abd10
Adam <aearanha@hotmail.com>
2021-09-15 20:20:15 -0400

    Added the sections for portfolio to App.js

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,8 @@ import './App.css';
 import Header from './components/Header/Header';
 import Intro from './components/Intro/Intro';
 import Projects from './components/Projects/Projects';
+import Skills from './components/Skills/Skills';
+import Contact from './components/Contact/Contact';
 
 
 function App() {
@@ -12,7 +14,9 @@ function App() {
 
       <Header />
       <Intro />
+      <Skills />
       <Projects />
+      <Contact />
 
     </div>
   );
