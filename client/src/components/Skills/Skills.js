c10b2e8ad29d1475f12b58883511803bf3a64ad5
Adam <aearanha@hotmail.com>
2021-09-16 23:23:49 -0400

    Added section name

--- client/src/components/Skills/Skills.js
+++ client/src/components/Skills/Skills.js
@@ -3,12 +3,12 @@ import React from 'react';
 export default function Skills() {
     return (
         <div className='skills' id='skills'>
-
-
+            <h2 className='section-name'>Skills</h2>
+            <div className='container-shadow'></div>
             <div className='container'>
 
             </div>
-            <div className='container-shadow'></div>
+
         </div>
     )
 }
