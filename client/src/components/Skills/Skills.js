55f6d251673820eb69f1c8ef58bca8957f657eb3
Adam <aearanha@hotmail.com>
2021-09-16 22:19:06 -0400

    Trying out different section shadow effect

--- client/src/components/Skills/Skills.js
+++ client/src/components/Skills/Skills.js
@@ -3,9 +3,12 @@ import React from 'react';
 export default function Skills() {
     return (
         <div className='skills' id='skills'>
+
+
             <div className='container'>
 
             </div>
+            <div className='container-shadow'></div>
         </div>
     )
 }
