68de7518a4b761f3908d27235acd8b52b5947633
Adam <aearanha@hotmail.com>
2021-09-18 14:54:15 -0400

    Adjusted Intro js to be more responsive

--- client/src/components/Intro/Intro.js
+++ client/src/components/Intro/Intro.js
@@ -3,26 +3,22 @@ import resume from '../../Assets/ResumeV1.pdf';
 import { ReactComponent as ArrowSVG } from '../assets/arrow-down.svg';
 
 
-
 export default function Intro() {
     return (
         <div className="intro" id='intro'>
-            <h2 className='section-name'>About Me</h2>
+            <div className='section-title'>
+                <h2 className='section-name'>About Me</h2>
+            </div>
             <div className='container'>
-                <div className='profile intro-section card-extraTall card-skinny'>
-                    <div className='profile-background-offset'></div>
-                    <div className='profile-background'>
+                <div className='picture-section'>
+                    <div className='picture-background-shadow'></div>
+                    <div className='picture-background'></div>
 
-                    </div>
                 </div>
-                <div className='  intro-section card-tall card-fat'>
-
-                    <p className='name'>Adam Aranha</p>
-                    <p className='description'>
-                        A Full Stack Web Developer passionate about creating interactive web experiences and applications.
-                    </p>
+                <div className='description-section'>
+                    <p> A Full Stack Web Developer passionate about creating interactive web experiences and applications.</p>
                 </div>
-                <div className='intro-section card-average card-fat'>
+                <div className='links-section'>
                     <a className="resume" href={resume} target="_blank" rel="noreferrer">Resume</a>
                 </div>
             </div>
