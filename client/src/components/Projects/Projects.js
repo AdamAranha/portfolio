c3b7a83512dd2f24ec68a62bf89afb4e5a27e9f4
Adam <aearanha@hotmail.com>
2021-09-18 15:36:35 -0400

    Redoing section

--- client/src/components/Projects/Projects.js
+++ client/src/components/Projects/Projects.js
@@ -5,11 +5,10 @@ export default function Projects() {
         <div className="projects" id="projects">
             <h2 className='section-name'>Projects</h2>
             <div className='container'>
+                {/* <div className='project-box'></div>
                 <div className='project-box'></div>
                 <div className='project-box'></div>
-                <div className='project-box'></div>
-                <div className='project-box'></div>
-
+                <div className='project-box'></div> */}
             </div>
         </div>
     )
