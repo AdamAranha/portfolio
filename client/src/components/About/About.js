d6bcca1075f89880a63c5e903b3fce8e39ea2028
Adam <aearanha@hotmail.com>
2021-09-23 17:41:51 -0400

    Tested out night mode toggle here

--- client/src/components/About/About.js
+++ client/src/components/About/About.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import Dias from '../Dias/Dias';
 
 export default function About() {
     return (
         <div className='about-section'>
-
+            <Dias />
         </div>
     )
 }
